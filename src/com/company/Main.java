698ae61910fc90e719dd20c69a81f85393ce8123
George Dudar <nurbolotbgk@gmail.com>
2023-03-01 17:42:40 +0600

    Adding two java files

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+//(импортируем новый класс который называется Scanner - этот класс позволяет получать данные от пользователя,
+// обращаемся к java, дальше обращаемся к util, дальше обращаемся к классу Scanner)
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
